63c607c4a6b71fc53b65e405da6f11e99838f92b
V-Swathi3398 <128344718+V-Swathi3398@users.noreply.github.com>
2023-03-31 12:38:45 +0530

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	
+	char Name[10];
+	int  Rollno;
+       float CGPA;
+
+       printf("enter the Name :");
+       scanf("%s",Name);
+       printf("enter the  Rollno:");
+       scanf("%d",&Rollno);
+       printf("enter the CGPA :");
+       scanf("%f",&CGPA);
+
+       return 0;
+}
+
