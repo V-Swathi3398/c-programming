de2ad7285c2ed12ce0e1c90565c70fd1d3907393
V-Swathi3398 <128344718+V-Swathi3398@users.noreply.github.com>
2023-04-19 13:02:26 +0530

    Add files via upload

---
+++ 04_loops/04_for/04_for.c
@@ -0,0 +1,31 @@
+/*********************************************************************************************
+ * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
+ * this source code, object code, or executable code (collectively, Software), is granted only
+ * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
+ * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
+ * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
+ * For further information, contact community.ruggedboard.com / info@ruggedboard.com
+ *
+ * Application:
+ * Brief:
+ * Author: Sanketh J H
+ * Title: Trainee
+ * Last Modified Date: 24.03.2023
+ *
+/*********************************************************************************************/
+
+// Header File
+#include <stdio.h>
+
+int main()
+{
+     int num = 10;
+     do
+     {
+          printf("num=%d\n", num);
+          num++;
+     } while (num <= 20);
+     return 0;
+}
+
+// Program End
